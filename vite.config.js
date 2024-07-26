af672fe28776a08958b3cc446b6b131f2979ff8a
Eric Chen <eric.chen0121@gmail.com>
2024-07-26 15:05:46 -0700

    test bun deploy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/litpicks/',
   plugins: [react()],
+  base: '/litpicks/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
